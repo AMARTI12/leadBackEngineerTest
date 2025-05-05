a1e9b581c7120478c2c178bb9e4be85f8171af44
Fredy A Martinez <freddyalex1272.fm@gmail.com>
2025-05-04 22:40:39 -0500

    feat: Se agrega esquema de proyecto generado por Spring Initializr

---
+++ src/main/java/com/dev/leadBackEngineerTest/LeadBackEngineerTestApplication.java
@@ -0,0 +1,13 @@
+package com.dev.leadBackEngineerTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadBackEngineerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadBackEngineerTestApplication.class, args);
+	}
+
+}
