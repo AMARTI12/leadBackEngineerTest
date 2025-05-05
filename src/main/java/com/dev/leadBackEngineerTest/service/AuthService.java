3eaea7899ef8ffe79c44367ee3b121cb2b4c818c
Fredy A Martinez <freddyalex1272.fm@gmail.com>
2025-05-05 10:18:03 -0500

    feat: Se agregan las nuevas dependencias para uso de JWT, asi mismo se agrega el servicio de autenticacion basado en la doc oficial para uso de JWT

---
+++ src/main/java/com/dev/leadBackEngineerTest/service/AuthService.java
@@ -0,0 +1,53 @@
+package com.dev.leadBackEngineerTest.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class AuthService {
+
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt.expiration}")
+    private long jwtExpirationMs;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+    public boolean validateToken(String token, String username) {
+        final String extractedUsername = extractUsername(token);
+        Date expiration = Jwts.parser()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+        return (extractedUsername.equals(username) && expiration.after(new Date()));
+    }
+
+    public String extractUsername(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
+}
