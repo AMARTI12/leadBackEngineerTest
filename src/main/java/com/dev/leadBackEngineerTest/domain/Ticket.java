d66c60441593f234cf1b46b916dd5bde5424a1c5
Fredy A Martinez <freddyalex1272.fm@gmail.com>
2025-05-05 00:24:10 -0500

    Se asocia el usuario en Ticket de acuerdo a la Entidad Relacion solicitada, se genera el ctor y getter & setter

--- src/main/java/com/dev/leadBackEngineerTest/domain/Ticket.java
+++ src/main/java/com/dev/leadBackEngineerTest/domain/Ticket.java
@@ -3,6 +3,7 @@ package com.dev.leadBackEngineerTest.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.annotations.UuidGenerator;
 import jakarta.validation.constraints.Size;
 
@@ -21,6 +22,13 @@ public class Ticket {
     @Size(max = 500)
     private String Description;
 
+    @Enumerated(EnumType.STRING)
+    private TicketStatus status;
+
+    @NotNull
+    @ManyToOne
+    private User user;
+
     private LocalDateTime CreatedAt;
     private LocalDateTime UpdatedAt;
 
@@ -32,5 +40,62 @@ public class Ticket {
     @PreUpdate
     protected void onUpdate() {
         UpdatedAt = LocalDateTime.now();
+    }
+
+    public Ticket(UUID id, String description, TicketStatus status, User user, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        Id = id;
+        Description = description;
+        this.status = status;
+        this.user = user;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
+    }
+
+    public UUID getId() {
+        return Id;
+    }
+
+    public void setId(UUID id) {
+        Id = id;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public TicketStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TicketStatus status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return CreatedAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        CreatedAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return UpdatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        UpdatedAt = updatedAt;
     }
 }
